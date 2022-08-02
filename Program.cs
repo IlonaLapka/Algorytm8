f44751733053acc317b7ba601406c138288a963f
Ilona Lapka <ilona2000123@wp.pl>
2022-08-02 21:01:37 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Algorytm8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Service service = new Service();
+            Console.WriteLine("Podaj zdanie: ");
+            string sentence = Console.ReadLine();
+            bool correct = false;
+
+            if (service.IsBeginningCorrect(sentence))
+            {
+                int startWordIndex = 0;
+                for (int i = 0; i < sentence.Length; i++)
+                {
+                    if (sentence[i] == ' ')
+                    {
+                        if (sentence[i + 1] != ' ' && service.IsWordCorrect(sentence.Substring(startWordIndex, i - startWordIndex), true))
+                        {
+                            startWordIndex = i + 1;
+                            correct = true;
+                        }
+                        else
+                        {
+                            correct = false;
+                            break;
+                        }
+                    }
+                    else if(i == sentence.Length - 1)
+                    {
+                        if (!service.IsWordCorrect(sentence.Substring(startWordIndex, i - startWordIndex + 1), false))
+                        {
+                            correct = false;
+                        }
+                    }
+                }
+
+            }
+
+            if (correct)
+            {
+                Console.WriteLine("To zdanie jest poprawne");
+            }
+            else
+            {
+                Console.WriteLine("To zdanie jest błędne");
+            }
+            
+        }
+    }
+}
---
+++ Service.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorytm8
+{
+    public class Service
+    {
+        public List<char> SpecialAtMiddle { get; set; } = new List<char>() { ':', ',', ';' };
+        public List<char> SpecialAtEnd { get; set; } = new List<char>() { '?', '!', '.' };
+        public bool IsBeginningCorrect(string beginning)
+        {
+            if(65 <= (int)beginning[0] && (int)beginning[0] <= 90)
+            {
+                if(97 <= (int)beginning[1] && (int)beginning[1] <= 122 || (int)beginning[1] == 127)
+                {
+                    return true;
+                }
+                else return false;
+            }
+            else return false;
+        }
+
+        public bool IsWordCorrect(string word, bool atMiddle)
+        {
+            if (65 <= (int)word[0] && (int)word[0] <= 90 || 
+                97 <= (int)word[0] && (int)word[0] <= 122)
+            {
+                for (int i = 1; i < word.Length; i++)
+                {
+                    if (i == word.Length - 1 && !atMiddle)
+                    {
+                        return IsSpecialCharInWordAtEndCorrect(word[i]);
+                    }
+                    if (97 > (int)word[i] || (int)word[i] > 122)
+                    {
+                        if(i == word.Length - 1)
+                        {
+                            return IsSpecialCharInWordAtMiddleCorrect(word[i]);
+                        }
+                        return false;
+                    }
+                    
+                }
+            }
+            else return false;
+
+            return true;
+        }
+
+        public bool IsSpecialCharInWordAtMiddleCorrect(char character)
+        {
+            if(SpecialAtMiddle.Contains(character)) return true;
+            else return false;
+        }
+
+        public bool IsSpecialCharInWordAtEndCorrect(char character)
+        {
+            if(SpecialAtEnd.Contains(character)) return true;
+            else return false;
+        }
+    }
+}
